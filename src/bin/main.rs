4c20a4657b13ea53ae7039b2dc011e7ea847dd54
krotera <01101011@tuta.io>
2020-11-30 14:53:19 -0500

    implement serial and parallel versions of mandelbrot computation (adapted from image crate's fractal example code)

---
+++ src/bin/main.rs
@@ -0,0 +1,76 @@
+use std::sync::{Arc, Mutex};
+
+use image;
+use num_complex;
+
+fn compute_pixel(imgbuf: Arc<Mutex<image::RgbImage>>, x: u32, y: u32, scale_x: f32, scale_y: f32) {
+    let c_x = x as f32 * scale_x - 1.5;
+    let c_y = y as f32 * scale_y - 1.5;
+
+    let c = num_complex::Complex::new(c_x, c_y);
+    let mut z = num_complex::Complex::new(c_x, c_y);
+
+    let mut i = 0;
+    while i < 255 && z.norm() <= 2.0 {
+        z = z * z + c;
+        i += 1;
+    }
+
+    let mut imgbuf_mutex_guard = (*imgbuf).lock().unwrap();
+    let pixel = (*imgbuf_mutex_guard).get_pixel_mut(x, y);
+    let image::Rgb(data) = *pixel;
+    *pixel = image::Rgb([data[0], i as u8, data[2]]);
+}
+
+/// Compute result serially
+#[allow(dead_code)]
+fn serial(imgbuf: Arc<Mutex<image::RgbImage>>, img_x: u32, img_y: u32, scale_x: f32, scale_y: f32) {
+    for x in 0..img_x {
+        for y in 0..img_y {
+            compute_pixel(Arc::clone(&imgbuf), x, y, scale_x, scale_y);
+        }
+    }
+}
+
+/// Compute result with parallel threads
+fn parallel(imgbuf: Arc<Mutex<image::RgbImage>>, img_x: u32, img_y: u32, scale_x: f32, scale_y: f32) {
+    let pool = fractal::ThreadPool::new(10);
+
+    // A redundant loop to demonstrate reading image data
+    for x in 0..img_x {
+        for y in 0..img_y {
+            let imgbuf_inner_arc = Arc::clone(&imgbuf);
+
+            pool.execute(move || {
+                compute_pixel(imgbuf_inner_arc, x, y, scale_x, scale_y);
+            });
+        }
+    }
+}
+
+fn main() {
+    // image dimensions
+    let img_x = 10000;
+    let img_y = 10000;
+    // dimensions of the window on the complex plane
+    let complex_plane_x = 3.0;
+    let complex_plane_y = 3.0;
+    // scale_n = (complex plane displacement / pixel displacement), along the n axis
+    let scale_x = complex_plane_x / img_x as f32;
+    let scale_y = complex_plane_y / img_y as f32;
+
+    // Create a new ImgBuf with width: img_x and height: img_y
+    let imgbuf = Arc::new(Mutex::new(image::ImageBuffer::new(img_x, img_y)));
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in (*imgbuf.lock().unwrap()).enumerate_pixels_mut() {
+        let r = (0.3 * x as f32) as u8;
+        let b = (0.3 * y as f32) as u8;
+        *pixel = image::Rgb([r, 0, b]);
+    }
+    // serial(Arc::clone(&imgbuf), img_x, img_y, scale_x, scale_y);
+    parallel(Arc::clone(&imgbuf), img_x, img_y, scale_x, scale_y);
+
+    // Save the image as “fractal.png”, the format is deduced from the path
+    (*imgbuf.lock().unwrap()).save("fractal.png").unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,130 @@
+// This is the thread pool from the book:
+// https://doc.rust-lang.org/stable/book/ch20-00-final-project-a-web-server.html
+//
+// # usage
+//
+//     let pool = ThreadPool::new(4); // use 4 threads
+//
+//     pool.execute(|| {
+//         foo();
+//     });
+//
+// where `foo()` is the function to parallelize
+//
+// # example
+//
+// to execute `foo()` `N` times in parallel with 4 threads
+// where `foo()` increments a number, `i`:
+//
+//     fn foo(i: Arc<Mutex<i32>>) {
+//         *i.lock().unwrap() += 1;
+//     }
+//
+//     let i = Arc::new(Mutex::new(0));
+//     let pool = ThreadPool::new(4);
+//
+//     for _ in 0..N {
+//         let i_inner = Arc::clone(&i);
+//
+//         pool.execute(move || {
+//             foo(i_inner);
+//         });
+//     }
+use std::thread;
+use std::sync::mpsc;
+use std::sync::Arc; // lets multiple Workers own the receiver
+use std::sync::Mutex; // ensures that only one Worker gets a job from the receiver at a time
+
+/// Task to execute per thread
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+/// Wrapper for Job, indicating further work or shutdown
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
+/// Creates a thread and waits for Messages
+#[allow(dead_code)]
+pub struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    job();
+                }
+                Message::Terminate => {
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Message>,
+}
+
+impl ThreadPool {
+    /// Create a new ThreadPool
+    ///
+    /// The size is the number of threads in the pool, with each thread stored inside a Worker.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is zero.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
+
+    /// Take a task as a closure and send it in a Message to the Workers
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+        {
+            let job = Box::new(f);
+
+            self.sender.send(Message::NewJob(job)).unwrap();
+        }
+}
+
+// when the pool is dropped, our threads should all join to make sure they finish their work.
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        for worker in &mut self.workers {
+            if let Some(thread) = worker.thread.take() {
+                // take ownership of the Worker's thread and have it finish
+                thread.join().unwrap();
+            }
+            // if it's None, this Worker has already had its thread cleaned up
+        }
+    }
+}
